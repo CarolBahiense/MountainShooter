29f2cc51445a33c8a21adb22a71f1673ce7f3415
Carol <artdraw.edit@gmail.com>
2025-03-14 12:15:31 -0300

    Creating player 1 and 2, making the movement of them. Creating Enemys and config them.

--- Code/Const.py
+++ Code/Const.py
@@ -1,13 +1,11 @@
+import pygame
+
+#c
 COLOR_ORANGE = (255, 128, 0)
 COLOR_WHITE = (255,255,255)
 COLOR_YELLOW = (255,255,128)
 
-MENU_OPTION =('NEW GAME 1P',
-              'NEW GAME 2P - COOPERATIVE',
-              'NEW GAME 2P - COMPETITIVE',
-              'SCORE',
-              'EXIT')
-
+#e
 ENTITY_SPEED = {
     'Level1Bg0' : 0,
     'Level1Bg1' : 1,
@@ -16,7 +14,45 @@ ENTITY_SPEED = {
     'Level1Bg4' : 4,
     'Level1Bg5' : 5,
     'Level1Bg6' : 6,
+    'Player1' : 3,
+    'Player2' : 3,
+    'Enemy1' : 2,
+    'Enemy2' : 1
+}
+EVENT_ENEMY = pygame.USEREVENT + 1
+
+#m
+MENU_OPTION =('NEW GAME 1P',
+              'NEW GAME 2P - COOPERATIVE',
+              'NEW GAME 2P - COMPETITIVE',
+              'SCORE',
+              'EXIT')
+
+#p
+PLAYER_KEY_UP = {
+    'Player1' : pygame.K_UP,
+    'Player2' : pygame.K_w
+}
+PLAYER_KEY_DOWN = {
+    'Player1' : pygame.K_DOWN,
+    'Player2' : pygame.K_s
+}
+PLAYER_KEY_LEFT = {
+    'Player1' : pygame.K_LEFT,
+    'Player2' : pygame.K_a
+}
+PLAYER_KEY_RIGHT = {
+    'Player1' : pygame.K_RIGHT,
+    'Player2' : pygame.K_d
+}
+PLAYER_KEY_SHOOT = {
+    'Player1' : pygame.K_RCTRL,
+    'Player2' : pygame.K_LCTRL,
 }
 
+#s
+SPAWN_TIME = 4000
+
+#w
 WIN_WIDTH = 576
 WIN_HEIGHT =324
